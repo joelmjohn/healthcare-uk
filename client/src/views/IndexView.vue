1c05a301871b7751109a563a5df932131a12acaf
Joel John <joel.m@techversantinfo.com>
2023-05-04 17:54:03 +0530

    cleaned pages

---
+++ client/src/views/IndexView.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="IndexView">
+        <b-container>
+            <h1>
+                <b>Foreignmentor Coming Soon!</b>
+            </h1>
+        </b-container>
+    </div>
+</template>
+  
+<script>
+// @ is an alias to /src
+
+
+export default {
+    name: 'IndexView',
+    components: {
+
+    }
+}
+</script>
+  
