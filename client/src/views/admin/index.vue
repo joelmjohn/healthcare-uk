6740c52de679fc8d24538af2f804347b0f9979f4
Joel John <joel.m@techversantinfo.com>
2023-05-08 13:14:58 +0530

    wip

---
+++ client/src/views/admin/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <b-alert show variant="primary">
+            Welcome to admin page
+        </b-alert>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'admin',
+    data() {
+        return {
+            root: process.env.VUE_APP_ROOT_API
+        }
+    },
+    created(){
+        alert('created'+this.root);
+    },
+    mounted(){
+        alert('mounted'+this.root);
+    }
+
+}
+
+
+</script>
+
+<style scoped></style>
