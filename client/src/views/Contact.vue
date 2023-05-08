fbca65ae114095cc25d25b48d3289d494868cd26
RemyaRajA1998 <remya@techversantinfo.com>
2023-05-08 11:34:36 +0530

    Updated Views and Components

---
+++ client/src/views/Contact.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Footer from "../components/Footer.vue";
+
+export default {
+  name: "Contact",
+  components: {
+    Footer,
+  },
+};
+</script>
+
+<style>
+</style>
