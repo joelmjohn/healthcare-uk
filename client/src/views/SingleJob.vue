dc3a6560f6490c0885bb00a484f698fe4b093ea7
RemyaRajA1998 <remya@techversantinfo.com>
2023-05-09 17:03:00 +0530

    Added Users List

--- client/src/views/SingleJob.vue
+++ client/src/views/SingleJob.vue
@@ -1,20 +1,16 @@
 <template>
   <div>
     <Header />
-    <SingleJob/>
   </div>
 </template>
 
 <script>
 import Header from "../components/Header.vue";
-import SingleJob from "../components/SingleJob.vue";
-import Footer from "../components/Footer.vue";
 
 export default {
   name: "SingleJob",
   components: {
     Header,
-    SingleJob,
   },
 };
 </script>
