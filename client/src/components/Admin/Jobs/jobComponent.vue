c6870ba36eb765539f4d73b6187c7a5de6d1c0d6
RemyaRajA1998 <remya@techversantinfo.com>
2023-06-20 11:00:40 +0530

    updated Vacancy and Experience required Modal issues

--- client/src/components/Admin/Jobs/jobComponent.vue
+++ client/src/components/Admin/Jobs/jobComponent.vue
@@ -1,291 +1,256 @@
 <template>
-  <b-modal
-    size="xl"
-    :title="actionType"
-    v-model="jobModal"
-    centered
-    @ok="handleCreate"
-    @cancel="cancelCreateModal"
-    @close="closeCreateModal"
-    :no-close-on-backdrop="true"
-  >
-    <b-container>
-      <b-row>
-        <b-col>
-          <label>Job Name</label>
-          <b-form-input
-            required
-            v-model="jobDetails.jobName"
-            placeholder="Enter Job Name"
-            trim
-          ></b-form-input
-        ></b-col>
-        <b-col>
-          <label>Job Description</label>
-          <b-form-textarea
-            class="rows-sm"
-            required
-            id="textarea"
-            v-model="jobDetails.jobDescription"
-            placeholder="Enter Job Description"
-          ></b-form-textarea
-        ></b-col>
-        <b-col>
-          <label>Company Name</label>
-          <b-form-input
-            required
-            v-model="jobDetails.companyName"
-            placeholder="Enter Company Name"
-            trim
-          ></b-form-input></b-col
-      ></b-row>
-      <b-row>
-        <b-col>
-          <label>Company Description</label>
-          <b-form-textarea
-            required
-            id="textarea"
-            v-model="jobDetails.companyDescription"
-            placeholder="Enter Company Description"
-          ></b-form-textarea
-        ></b-col>
-        <b-col cols="4">
-          <label>Job status</label>
-          <select
-            required
-            v-model="statusSelected"
-            class="custom-select custom-select-sm"
-            aria-label="Default select example"
-          >
-            <option value="" disabled>Select</option>
-            <option v-for="(statusValue, index) in status" :key="index">
-              {{ statusValue }}
-            </option>
-          </select></b-col
-        >
+    <b-modal size="xl" :title="actionType" v-model="jobModal" centered @ok="handleModal" @cancel="cancelJobModal"
+        @close="closeJobModal" :no-close-on-backdrop="true">
+        <b-container>{{ countryd }}
+            <b-row>
+                <b-col>
+                    <label>Job Name</label>
+                    <b-form-input required v-model="jobDetails.jobName" placeholder="Enter Job Name"
+                        trim></b-form-input></b-col>
+                <b-col>
+                    <label>Job Description</label>
+                    <b-form-textarea class="rows-sm" required id="textarea" v-model="jobDetails.jobDescription"
+                        placeholder="Enter Job Description"></b-form-textarea></b-col>
+                <b-col>
+                    <label>Company Name</label>
+                    <b-form-input required v-model="jobDetails.companyName" placeholder="Enter Company Name"
+                        trim></b-form-input></b-col></b-row>
+            <b-row>
+                <b-col>
+                    <label>Company Description</label>
+                    <b-form-textarea required id="textarea" v-model="jobDetails.companyDescription"
+                        placeholder="Enter Company Description"></b-form-textarea></b-col>
+                <b-col cols="4">
+                    <label>Job status</label>
+                    <select required v-model="jobDetails.status" class="custom-select custom-select-sm"
+                        aria-label="Default select example">
+                        <option value="" disabled>Select</option>
+                        <option v-for="(statusValue, index) in statusSelected" :key="index">
+                            {{ statusValue }}
+                        </option>
+                    </select></b-col>
 
-        <b-col>
-          <label>Industry</label>
-          <b-form-input
-            required
-            v-model="jobDetails.industryType"
-            placeholder="Enter Industry"
-          ></b-form-input
-        ></b-col>
-      </b-row>
-      <b-row>
-        <b-col>
-          <label>Vacancy</label>
-          <b-form-input
-            :state="vacancyCheck"
-            @input="validateVacancy"
-            required
-            v-model="jobDetails.vacancy"
-            placeholder="Enter Vacancy"
-          ></b-form-input
-        ></b-col>
-        <b-col>
-          <label>Employment Type</label>
-          <select
-            required
-            v-model="jobDetails.employmentTypeSelected"
-            class="custom-select custom-select-sm"
-            aria-label="Default select example"
-          >
-            <option value="" disabled>Select</option>
-            <option v-for="(type, index) in employmentType" :key="index">
-              {{ type }}
-            </option>
-          </select>
-        </b-col>
-        <b-col>
-          <label>Experience Required</label>
-          <b-form-input
-            @input="validateExperience"
-            :state="experienceCheck"
-            required
-            v-model="jobDetails.experienceRequired"
-            placeholder="Enter Experience"
-          ></b-form-input
-        ></b-col>
-      </b-row>
-      <b-row>
-        <b-col>
-          <label>Address</label>
-          <b-form-textarea
-            required
-            id="textarea"
-            v-model="jobDetails.address"
-            placeholder="Enter Address"
-          ></b-form-textarea
-        ></b-col>
+                <b-col>
+                    <label>Industry</label>
+                    <b-form-input required v-model="jobDetails.industryType"
+                        placeholder="Enter Industry"></b-form-input></b-col>
+            </b-row>
+            <b-row>
+                <b-col>
+                    <label>Vacancy</label>
+                    <b-form-input :state="vacancyCheck" required v-model="jobDetails.vacancy"
+                        placeholder="Enter Vacancy"></b-form-input></b-col>
+                <b-col>
+                    <label>Employment Type</label>
+                    <select required v-model="jobDetails.employmentType" class="custom-select custom-select-sm"
+                        aria-label="Default select example">
+                        <option value="" disabled>Select</option>
+                        <option v-for="(type, index) in employmentTypeSelected" :key="index">
+                            {{ type }}
+                        </option>
+                    </select>
+                </b-col>
+                <b-col>
+                    <label>Experience Required</label>
+                    <b-form-input :state="experienceCheck" required v-model="jobDetails.experienceRequired"
+                        placeholder="Enter Experience"></b-form-input></b-col>
+            </b-row>
+            <b-row>
+                <b-col>
+                    <label>Address</label>
+                    <b-form-textarea required id="textarea" v-model="jobDetails.address"
+                        placeholder="Enter Address"></b-form-textarea></b-col>
 
-        <b-col>
-          <label>Country</label>
-          <select
-            required
-            v-model="countrySelected"
-            class="custom-select custom-select-sm"
-            aria-label="Default select example"
-          >
-            <option value="" disabled>Select</option>
-            <option
-              v-for="(country, index) in countrys"
-              :value="country.id"
-              :key="index"
-            >
-              {{ country.name }} ({{ country.countryCode }})
-            </option>
-          </select></b-col
-        >
-        <b-col>
-          <label>Skills Required</label>
-          <multiselect
-            v-model="skillsRequired"
-            :options="skillsDetails"
-            :multiple="true"
-            :close-on-select="true"
-          >
-          </multiselect>
-        </b-col>
-      </b-row>
-      <b-row>
-        <b-col cols="4">
-          <label>Valid Till Date</label>
-          <b-form-datepicker
-            v-model="jobDetails.validTillDate"
-            class="mb-2"
-          ></b-form-datepicker
-        ></b-col>
-      </b-row>
-    </b-container>
-  </b-modal>
+                <b-col>
+                    <label>Country</label>
+                    <select required v-model="jobDetails.countrySelected" class="custom-select custom-select-sm"
+                        aria-label="Default select example">
+                        <option value="" disabled>Select</option>
+                        <option v-for="(country, index) in countrys" :value="country.id" :key="index">
+                            {{ country.name }} ({{ country.countryCode }})
+                        </option>
+                    </select></b-col>
+                <b-col>
+                    <label>Skills Required</label>
+                    <multiselect v-model="jobDetails.skillsRequired" :options="skillsDetails" :multiple="true"
+                        :close-on-select="true">
+                    </multiselect>
+                </b-col>
+            </b-row>
+            <b-row>
+                <b-col cols="4">
+                    <label>Valid Till Date</label>
+                    <b-form-datepicker v-model="jobDetails.validTillDate" class="mb-2"></b-form-datepicker></b-col>
+            </b-row>
+        </b-container>
+    </b-modal>
 </template>
-
+  
 <script>
 import Multiselect from "vue-multiselect";
 export default {
-  components: { Multiselect },
-  data() {
-    return {
-      jobDetails: {
-        jobName: "",
-        jobDescription: "",
-        companyName: "",
-        companyDescription: "",
-        address: "",
-        vacancy: "",
-        experienceRequired: "",
-        industryType: "",
-        validTillDate: "",
-        employmentTypeSelected: "",
-      },
-      employmentType: ["FULL TIME", "PART TIME"],
-      skillsRequired: "",
-      skillsDetails: [
-        "Situational Awareness",
-        "Empathy",
-        "Leadership",
-        "Teamwork",
-      ],
-      countryId: "",
-      countrySelected: "",
-      status: ["ACTIVE", "EXPIRED"],
-      statusSelected: "",
-      adminId: localStorage.getItem("adminId"),
-      vacancyCheck: false,
-      experienceCheck: false,
-    };
-  },
+    components: { Multiselect },
+    data() {
+        return {
+            jobDetails: {
+                jobName: "",
+                jobDescription: "",
+                companyName: "",
+                companyDescription: "",
+                address: "",
+                vacancy: "",
+                experienceRequired: "",
+                industryType: "",
+                validTillDate: "",
+                employmentType: "",
+                skillsRequired: "",
+                status: "",
+                countrySelected: "",
+            },
+            employmentTypeSelected: ["FULL TIME", "PART TIME"],
+            skillsDetails: [
+                "Situational Awareness",
+                "Empathy",
+                "Leadership",
+                "Teamwork",
+            ],
+            countryId: "",
+            statusSelected: ["ACTIVE", "EXPIRED"],
+            adminId: localStorage.getItem("adminId"),
+            vacancyCheck: false,
+            experienceCheck: false,
+        };
+    },
+    props: {
+        jobModal: { required: true, type: Boolean },
+        countrys: { required: true, type: Array },
+        actionType: {
+            type: String,
+            default: "Create"
+        },
+        newData: { required: true, type: Object },
+        countryd: { required: true, type: String }
+    },
+    watch: {
 
-  props: {
-    jobModal: { required: true, type: Boolean },
-    countrys: { required: true, type: Array },
-    actionType: {
-      type: String,
-      default: "Create",
-    },
-  },
-  methods: {
-    validateVacancy() {
-      if (
-        !Number.isInteger(Number(this.jobDetails.vacancy)) ||
-        this.jobDetails.vacancy == ""
-      ) {
-        this.vacancyCheck = false;
-      } else {
-        this.vacancyCheck = true;
-      }
-    },
-    validateExperience() {
-      if (
-        !Number.isInteger(Number(this.jobDetails.experienceRequired)) ||
-        this.jobDetails.experienceRequired == ""
-      ) {
-        this.experienceCheck = false;
-      } else {
-        this.experienceCheck = true;
-      }
-    },
-    toggleStatus() {
-      this.status = !this.status;
-    },
-    handleCreate(evt) {
-      evt.preventDefault();
-      const data = {
-        jobName: this.jobDetails.jobName,
-        jobDescription: this.jobDetails.jobDescription,
-        companyName: this.jobDetails.companyName,
-        companyDescription: this.jobDetails.companyDescription,
-        vacancy: this.jobDetails.vacancy,
-        address: this.jobDetails.address,
-        experienceRequired: this.jobDetails.experienceRequired,
-        skillsRequired: this.skillsRequired,
-        industryType: this.jobDetails.industryType,
-        employmentType: this.jobDetails.employmentTypeSelected,
-        validTillDate: this.jobDetails.validTillDate,
-        countryId: this.countrySelected,
-        status: this.statusSelected,
-        adminId: this.adminId,
-      };
-      const isEmpty = Object.values(data).some((value) => value === "");
-      if (isEmpty) {
-        this.toast("Error", "Please fill all the details", "danger");
-      } else if (
-        this.vacancyCheck === false ||
-        this.experienceCheck === false
-      ) {
-        this.toast("Error", "Please enter a number", "danger");
-      } else {
-        this.$emit("displayJobs", data);
-        this.$emit("closeJobModal");
-        this.countrySelected = "";
-        this.statusSelected = "";
-        this.skillsRequired = "";
-        this.vacancyCheck = false;
-        this.experienceCheck = false;
-        for (const key in this.jobDetails) {
-          delete this.jobDetails[key];
+        newData() {
+            this.jobDetails = this.newData
+
+            console.log(this.jobDetails);
+        },
+        //inorder to check the value of vacancy and experience required is a number or not
+        "jobDetails.vacancy": function (data) {
+            if (this.actionType === "Create") {
+                if (!Number.isInteger(Number(data)) || this.jobDetails.vacancy == "") {
+                    this.vacancyCheck = false
+                }
+                else {
+                    this.vacancyCheck = true
+                }
+            }
+            else {
+                if (this.actionType === "Update") {
+                    if (!Number.isInteger(Number(data)) || this.jobDetails.vacancy == "") {
+                        this.vacancyCheck = false
+                    }
+                    else {
+                        this.vacancyCheck = true
+                    }
+                }
+            }
+        },
+        "jobDetails.experienceRequired": function (data) {
+            if (this.actionType === "Create") {
+                if (!Number.isInteger(Number(data)) || this.jobDetails.experienceRequired == "") {
+                    this.experienceCheck = false
+                }
+                else {
+                    this.experienceCheck = true
+                }
+            }
+            else {
+                if (this.actionType === "Update") {
+                    if (!Number.isInteger(Number(data)) || this.jobDetails.experienceRequired == "") {
+                        this.experienceCheck = false
+                    }
+                    else {
+                        this.experienceCheck = true
+                    }
+                }
+            }
+            //inorder to check the value of vacancy and experience required is a number or not
+
         }
-      }
     },
-    cancelCreateModal(evt) {
-      evt.preventDefault();
-      this.$emit("closeJobModal");
+    methods: {
+        toggleStatus() {
+            this.status = !this.status;
+        },
+        handleModal(evt) {
+            evt.preventDefault();
+            if (this.actionType === 'Create') {
+                const datas = {
+                    jobName: this.jobDetails.jobName,
+                    jobDescription: this.jobDetails.jobDescription,
+                    companyName: this.jobDetails.companyName,
+                    companyDescription: this.jobDetails.companyDescription,
+                    vacancy: this.jobDetails.vacancy,
+                    address: this.jobDetails.address,
+                    experienceRequired: this.jobDetails.experienceRequired,
+                    skillsRequired: this.jobDetails.skillsRequired,
+                    industryType: this.jobDetails.industryType,
+                    employmentType: this.jobDetails.employmentType,
+                    validTillDate: this.jobDetails.validTillDate,
+                    status: this.jobDetails.status,
+                    adminId: this.adminId,
+                    countryId: this.jobDetails.countrySelected
+                };
+
+                console.log(datas);
+                const isEmpty = Object.values(datas).some((value) => value === undefined);
+                console.log(isEmpty);
+                if (isEmpty) {
+                    this.toast("Error", "Please fill all the details", "danger");
+                }
+                else {
+                    this.$emit("displayJobs", datas);
+                    this.$emit("closeJobModal");
+                    for (const key in this.jobDetails) {
+                        delete this.jobDetails[key];
+                    }
+                }
+            }
+            else if (this.actionType === 'Update') {
+                const isEmpty = Object.values(this.newData).some((value) => value === "");
+                if (isEmpty) {
+                    this.toast("Error", "Please fill all the details", "danger");
+                }
+                else {
+                    this.$emit("newDataDetails", this.newData);
+                    this.$emit("closeJobModal");
+                }
+
+            }
+
+        },
+        cancelJobModal(evt) {
+            evt.preventDefault();
+            this.$emit("closeJobModal");
+        },
+        closeJobModal(evt) {
+            evt.preventDefault();
+            this.$emit("closeJobModal");
+        },
+        toast(title, msg, variant) {
+            this.$bvToast.toast(msg, {
+                title: title,
+                variant: variant,
+                solid: true,
+            });
+        },
     },
-    closeCreateModal(evt) {
-      evt.preventDefault();
-      this.$emit("closeJobModal");
-    },
-    toast(title, msg, variant) {
-      this.$bvToast.toast(msg, {
-        title: title,
-        variant: variant,
-        solid: true,
-      });
-    },
-  },
 };
 </script>
-
+  
 <style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
+  
